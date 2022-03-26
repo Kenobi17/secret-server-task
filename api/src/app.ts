17a20e771000ee206e5cc2c0d114c5ad69655540
Kenobi17 <matiascorrea420@gmail.com>
2022-03-26 19:30:03 -0300

    Add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,13 @@
 import { config } from 'dotenv';
 config();
 import express from 'express';
+import cors from 'cors';
 import secretRouter from './routes/secret.routes';
 import './db';
 
 const app = express();
 
+app.use(cors({ origin: process.env.ORIGIN_URL || 'http://localhost:3000' }));
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
