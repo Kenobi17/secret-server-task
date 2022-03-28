35474d844932bdfa4e05d18dbe065bb06edd249a
Kenobi17 <matiascorrea420@gmail.com>
2022-03-28 15:27:03 -0300

    Dockerize API

--- api/src/db.ts
+++ api/src/db.ts
@@ -5,8 +5,12 @@ const url = process.env.MONGO_URL || 'mongodb://localhost:27017';
 const client: MongoClient = new MongoClient(url);
 
 (async function () {
-  await client.connect();
-  console.log('Connected to MongoDB');
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
 })();
 
 export default client;
